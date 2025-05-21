2332c098199faeafd6fdc5a6b1f1a86c7149704c
Andrii Salohub <andrejsalogub3@gmail.com>
2025-05-21 21:41:06 +0300

    fix: resolve issues for successful project deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,11 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import Header  from "@/components/Header";
+import Header from "@/components/Header";
 
 export const Route = createRootRoute({
-
   component: () => (
-    <>      
-      <Header/>
+    <>
+      <Header />
       <Outlet />
       <TanStackRouterDevtools />
     </>
