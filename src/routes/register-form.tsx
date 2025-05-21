a83f15cef017b47ba700dd84827a888e0a18a2ec
Vysotskyi Ihor <ihor.vysotskyi@nure.ua>
2025-05-21 21:23:48 +0300

    refactor: changed file names, moved self made components from \ui in \components, apdated their imports.

--- src/routes/register-form.tsx
+++ src/routes/register-form.tsx
@@ -3,7 +3,3 @@ import RegisterForm from '@/pages/RegisterForm'
 export const Route = createFileRoute('/register-form')({
   component: () =><RegisterForm/>,
 })
-
-function RouteComponent() {
-  return <div>Hello "/register-form"!</div>
-}
