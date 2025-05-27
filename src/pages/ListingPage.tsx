28a248d8f4c44835a329a0efd31bc611c6b27361
Vysotskyi Ihor <ihor.vysotskyi@nure.ua>
2025-05-27 22:11:23 +0300

    fix

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -1,10 +1,10 @@
 // src/pages/ListingPage.tsx
 import ListingForm from "@/components/ListingForm";
 
-export default function ListingPage({ role, propertyId }: { role: string; propertyId: string }) {
+export default function ListingPage({propertyId }: {propertyId: string }) {
   return (
     <div className="flex gap-8 p-8 justify-center items-start">
-      <ListingForm role={role} propertyId = {propertyId} />
+      <ListingForm propertyId = {propertyId} />
     </div>
   );
 }
