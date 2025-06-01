545c79680ca5946b63a718004e305323f4f2dea4
Andrii Salohub <andrejsalogub3@gmail.com>
2025-06-01 14:51:58 +0300

    fix: add correct component for the listings page

--- src/routes/listings.tsx
+++ src/routes/listings.tsx
@@ -4,8 +4,8 @@
 //   component: () => <ListingsMainPage/>,
 // });
 
-import { createFileRoute } from '@tanstack/react-router'
-import AllListings from '@/pages/AllListingsPage';
-export const Route = createFileRoute('/listings')({
-  component: () => <AllListings/>,
+import { createFileRoute } from "@tanstack/react-router";
+import ListingsMainPage from "@/pages/ListingsMainPage";
+export const Route = createFileRoute("/listings")({
+  component: () => <ListingsMainPage />,
 });
